cd6073e785225ff8f68a4416289f5df9a3a2dd0d
Diane Guzman <117790100+dianeooty@users.noreply.github.com>
2023-03-27 11:19:38 -0700

    Updates: logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -87,7 +87,7 @@ d3.json(url).then(function (data) {
     console.log("Depths:", depths);
     console.log("Magnitudes:", mags);
 
-    // Create function with switch statements for color based on depth range
+    // Create function using switch statements to return color based on depth range
     function setColor(depth) {
         switch (true) {
             case depth > 90:
